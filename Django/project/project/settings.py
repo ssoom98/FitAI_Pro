2adcf18112aad541b464220385765e4548be2266
minjunK94 <fni123r4@gmail.com>
2025-02-26 12:47:09 +0900

    웹

--- Django/project/project/settings.py
+++ Django/project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "body_type",
     "diet_recommendation",
     "pose_analysis",
     "home",
