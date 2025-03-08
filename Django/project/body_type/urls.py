f9acff96d7c5987a36e41cb0269b611ad558eb88
ssoom98 <breathin9898@gmail.com>
2025-03-08 17:37:36 +0900

    'web_css'

--- Django/project/body_type/urls.py
+++ Django/project/body_type/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from .views import predict_body_type
 
+app_name = 'body_type'
+
 urlpatterns = [
     path("", predict_body_type, name="body_type_home"),  # 기본 페이지를 predict_body_type으로 설정
     path("predict/", predict_body_type, name="predict_body_type"),
--- Django/project/project/urls.py
+++ Django/project/project/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("body_type/", include("body_type.urls")),
     path('diet/', include('diet_recommendation.urls')),
     path('pose/', include('pose_analysis.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
